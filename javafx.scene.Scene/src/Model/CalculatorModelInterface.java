4e34ebf7f7c280369cda7b6ffff1971cc0c5054f
noeflandre <noeflandre@localhost>
2023-10-25 09:13:34 +0200

    Commented the Model Interface

--- javafx.scene.Scene/src/Model/CalculatorModelInterface.java
+++ javafx.scene.Scene/src/Model/CalculatorModelInterface.java
@@ -5,15 +5,61 @@ package Model;
  * It includes methods for manipulating a stack and performing arithmetic operations.
  */
 public interface CalculatorModelInterface {
-    void push(double value);
-    void pop();
-    void clear();
-    void swap();
-    void drop();
-    void add();
-    void subtract();
-    void divide();
-    void opposite();
-    void multiply();
-    void enter();
+	/**
+	 * Pushes the specified value onto the stack.
+	 *
+	 * @param value The value to be pushed onto the stack.
+	 */
+	void push(double value);
+
+	/**
+	 * Removes the top element from the stack and returns it.
+	 */
+	void pop();
+
+	/**
+	 * Clears the entire stack.
+	 */
+	void clear();
+
+	/**
+	 * Swaps the positions of the top two elements on the stack.
+	 */
+	void swap();
+
+	/**
+	 * Removes the top element from the stack without returning it.
+	 */
+	void drop();
+
+	/**
+	 * Adds the top two elements on the stack and pushes the result back onto the stack.
+	 */
+	void add();
+
+	/**
+	 * Subtracts the top element from the element below it and pushes the result onto the stack.
+	 */
+	void subtract();
+
+	/**
+	 * Divides the second-to-top element by the top element and pushes the result onto the stack.
+	 */
+	void divide();
+
+	/**
+	 * Changes the sign of the top element on the stack (makes it positive or negative).
+	 */
+	void opposite();
+
+	/**
+	 * Multiplies the top two elements on the stack and pushes the result onto the stack.
+	 */
+	void multiply();
+
+	/**
+	 * Indicates the completion of entering a numeric value onto the stack.
+	 */
+	void enter();
+
 }
